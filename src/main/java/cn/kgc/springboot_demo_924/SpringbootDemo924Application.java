ec97924af7df9a9a5c19f2d1c43d6233345eab17
丁允浩 <461746946@qq.com>
2019-09-25 15:56:24 +0800

    Initial commit spring boot项目

---
+++ src/main/java/cn/kgc/springboot_demo_924/SpringbootDemo924Application.java
@@ -0,0 +1,13 @@
+package cn.kgc.springboot_demo_924;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo924Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemo924Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/kgc/springboot_demo_924/SpringbootDemo924ApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.kgc.springboot_demo_924;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootDemo924ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
